d00c6e6666359eb01a05463697d6f1f1d8b3fd23
Giang Dang <giangdc2@fpt.com>
2026-06-30 18:53:40 +0700

    commit bài 9

--- src/tests/bai9.spec.ts
+++ src/tests/bai9.spec.ts
@@ -4,6 +4,26 @@ import { test, expect } from '@playwright/test';
     await page.goto('https://staging.tongdaiwifi.vn');
     await page.locator('[role="group"]').filter({ hasText: 'Combo Giga' }).getByRole('link', { name: 'Xem chi tiết' }).click();    
     await expect(page.getByText('Combo Giga')).toBeVisible();
+
+});
+  test('Đếm số lượng card ở block đầu tiên', async ({ page }) => {
+    await page.goto('https://staging.tongdaiwifi.vn');
+    const cardCount = await page.locator('[class="relative select-none"]').nth(0).locator('[data-slot="carousel-item"]').count()    ;
+    console.log(`Số lượng card trong block đầu tiên: ${cardCount}`);
+
+
+});
+  test('Kiểm tra card gói combo ở card đầu tiên', async ({ page }) => {
+    await page.goto('https://staging.tongdaiwifi.vn');
+    const card1 = await page.locator('[class="relative select-none"]').first() ;
+    expect(await card1.getByText('Combo Giga')).toBeVisible();
+});
+
+  test('Kiểm tra click đăng ký ngay gói Combo sky', async ({ page }) => {
+    await page.goto('https://staging.tongdaiwifi.vn');
+    const card2 = await page.locator('[class="relative select-none"]').nth(0);
+    const registerButton = await card2.getByRole('link', { name: 'Đăng ký ngay' });
+    await registerButton.click();
 });
 
  
